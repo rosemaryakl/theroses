a7a57399ab0e21d1e49956930d293ea84a6e32e3
rosemaryakl <61705133+rosemaryakl@users.noreply.github.com>
2022-10-21 11:45:40 +0100

    userinput class created

--- src/main/java/Game/UserInput.java
+++ src/main/java/Game/UserInput.java
@@ -2,6 +2,32 @@ package Game;
 
 public class UserInput {
 	
+	public void quit() {
+		System.out.println("Game over!");
+		System.exit(0);
+	}
+	
+	public void receiveMovement (char key){
+		
+		if(key == 'w') {
+			moveNorth(); // froom other team
+		}
+		else if (key == 'd') {
+			moveEast();
+		}
+		else if (key == 's') {
+			moveSouth();
+		}
+		else if (key == 'a') {
+			moveWest();
+		}
+		else (key == 'q'){
+			quit(); 
+		}
+	}
+	
+
+	
 	/* attribute
 	 *  - scanner object 
 	 *  
