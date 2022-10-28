9e0182838bfa651b67ce6ef4b9decbd25fe4473c
Jack Jorgensen <jackwjorgensen@gmail.com>
2022-10-28 12:26:48 +0100

    added test folder

---
+++ src/test/java/theroses_project/test_game.java
@@ -0,0 +1,24 @@
+package theroses_project;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class test_game {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
