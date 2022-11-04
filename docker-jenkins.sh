6c6914cce0e6f5b784a4bd607e98257fef5087f2
Remi Bahar <32700264+RemiBahar@users.noreply.github.com>
2022-11-04 11:17:15 +0000

    Update docker-jenkins.sh

--- docker-jenkins.sh
+++ docker-jenkins.sh
@@ -1,8 +1,8 @@
 # Build image (replaces existing image)
-sudo docker build -f dockerfile-jenkins . -t game
+docker build -f dockerfile-jenkins . -t game
 # Remove existing container so it can be replaced with updated one 
-sudo docker rm game-container
+docker rm game-container
 # Run container in interactive mode
 echo "d s a w d d d q" | {
-	sudo docker run --name game-container -i game
+	docker run --name game-container -i game
 }
